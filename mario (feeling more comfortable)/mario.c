319e6832d3c8e4880d65cda6ef01b9d55e985c43
Toktar Kametay <167887395+InfectedDuck@users.noreply.github.com>
2024-07-29 21:12:10 +0600

    Update mario.c

--- mario (feeling more comfortable)/mario.c
+++ mario (feeling more comfortable)/mario.c
@@ -1,28 +1,44 @@
-#include <stdio.h>
-#include <cs50.h>
+#include <stdio.h> // Include the standard input/output library for printing output
+#include <cs50.h> // Include the CS50 library for getting input from the user
+
 int main(void)
 {
-    int height;
+    int height; // Variable to store the height of the object (pyramid)
+
+    // Prompt the user to enter a height between 1 and 8 (inclusive)
     do
     {
-        height=get_int("Write the height of an object: ");
+        height = get_int("Write the height of an object: "); // Get the height from the user
     }
-    while ((height<1) || (height >8));
-    for (int i=1;i<=height;i++)
+    while (height < 1 || height > 8); // Repeat the prompt if the height is not within the valid range
+
+    // Loop to construct each row of the pyramid
+    for (int i = 1; i <= height; i++)
     {
-        for (int j=0;j<height-i;j++)
+        // Print spaces before the hashes for the left side of the pyramid
+        for (int j = 0; j < height - i; j++)
         {
-            printf(" ");
+            printf(" "); // Print a space
         }
-        for (int j=0;j<i;j++)
+
+        // Print hashes for the left side of the pyramid
+        for (int j = 0; j < i; j++)
         {
-            printf("#");
+            printf("#"); // Print a hash
         }
-        printf("  ");
-        for (int j=0;j<i;j++)
+
+        // Print two spaces between the two sides of the pyramid
+        printf("  "); // Print two spaces
+
+        // Print hashes for the right side of the pyramid
+        for (int j = 0; j < i; j++)
         {
-            printf("#");
+            printf("#"); // Print a hash
         }
+
+        // Move to the next line after completing the current row
         printf("\n");
     }
+
+    return 0; // Return 0 to indicate successful completion of the program
 }
